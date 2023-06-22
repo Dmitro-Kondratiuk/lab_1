4cfb86ba8887ec6cc5d44b05915ab67c11bfda9c
Dmitro Kondratiuk <d.kondratyuk@clearjunction.com>
2023-06-22 17:50:29 +0300

    Update lab_2.php

--- lab_2.php
+++ lab_2.php
@@ -1,6 +1,10 @@
 <?php
-$words = [];
-$file = fopen('words_alpha.txt', 'r');
+$totalTime = microtime(true);
+
+[$ms, $s] = explode(' ', microtime());
+$startMicroTime = $s * 1000 + round($ms * 1000);
+$words          = [];
+$file           = fopen('words_alpha.txt', 'r');
 while (($line = fgets($file)) !== false) {
     $word = trim($line);
     if (strlen($word) == 5 && strlen($word) == count(array_unique(str_split($word)))) {
@@ -9,18 +13,22 @@ while (($line = fgets($file)) !== false) {
 }
 fclose($file);
 
+
 function search_words($letters, $words, $used) {
+    $file_for_write = 'output.txt';
     if (empty($letters)) {
-        foreach ($used as $u) {
-            if (strlen($u) == 5) {
-                echo $u . "\n";
+        foreach ($used as $key => $u) {
+            if (5 !== strlen($u)) {
+                unset($used[$key]);
             }
         }
-        exit();
+        file_put_contents($file_for_write, implode(' ', $used) . "\n", FILE_APPEND);
+
+        return;
     }
     $counts = array_count_values(str_split(implode($words)));
     asort($counts);
-    $rarest = key($counts);
+    $rarest   = key($counts);
     $newWords = $words;
     if (count($letters) % 5 != 0) {
         $newWords[] = $rarest;
@@ -28,15 +36,20 @@ function search_words($letters, $words, $used) {
     foreach ($newWords as $word) {
         if (($word && $rarest !== null) && strpos($word, $rarest) !== false) {
             $newLetters = array_diff($letters, str_split($word));
-            $newWords = array_filter($words, function ($w) use ($word) {
+            $newWords   = array_filter($words, function ($w) use ($word) {
                 return !count(array_intersect(str_split($w), str_split($word)));
             });
-            $newUsed = $used;
-            $newUsed[] = $word;
+            $newUsed    = $used;
+            $newUsed[]  = $word;
             search_words($newLetters, $newWords, $newUsed);
         }
     }
 }
 
 search_words(range('a', 'z'), array_values($words), []);
+$totalTime = microtime(true) - $totalTime;
 
+[$ms, $s] = explode(' ', microtime());
+$endMicroTime = $s * 1000 + round($ms * 1000);
+
+echo 'Total time: ' . ($endMicroTime - $startMicroTime) . ' ms';
